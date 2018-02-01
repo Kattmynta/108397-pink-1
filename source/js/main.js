6f33c1b7089fe3bae1682b278026e7f24d24e16b
AK <nageneril@mudrapragram.qq>
2018-02-01 16:11:21 +0300

    Сетки главной страницы - до прайс-листа

--- source/js/main.js
+++ source/js/main.js
@@ -3,10 +3,10 @@ var menu = document.querySelector(".main-nav__list");
 
 toggle.addEventListener("click", function(event) {
   event.preventDefault();
-  if (menu.classList.contains("main-nav__list--hide")) {
-    menu.classList.remove("main-nav__list--hide");
+  if (menu.classList.contains("main-nav__list--compact")) {
+    menu.classList.remove("main-nav__list--compact");
   }
   else {
-    menu.classList.add("main-nav__list--hide");
+    menu.classList.add("main-nav__list--compact");
   }
 });
