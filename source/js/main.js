f5ad31cf5d4a8e496bb4a0773e3d2d3cc90257a4
Anna Kuleshova <soleil_opaque@mail.ru>
2018-02-07 23:32:30 +0300

    Прописаны стили для главного меню (частично)

--- source/js/main.js
+++ source/js/main.js
@@ -1,12 +1,22 @@
+var header = document.querySelector(".page-header");
 var toggle = document.querySelector(".main-nav__toggle");
 var menu = document.querySelector(".main-nav__list");
+
+header.classList.remove("page-header--nojs");
+header.classList.add("page-header--compact");
+menu.classList.remove("main-nav__list--nojs");
+menu.classList.add("main-nav__list--compact");
 
 toggle.addEventListener("click", function(event) {
   event.preventDefault();
   if (menu.classList.contains("main-nav__list--compact")) {
+    header.classList.remove("page-header--compact");
+    header.classList.add("page-header--full");
     menu.classList.remove("main-nav__list--compact");
   }
   else {
+    header.classList.add("page-header--compact");
+    header.classList.remove("page-header--full");
     menu.classList.add("main-nav__list--compact");
   }
 });
