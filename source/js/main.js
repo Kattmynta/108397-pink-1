4c26f515cc2334faf883ecbfa0ddc3f0015bfe23
AK <nageneril@mudrapragram.qq>
2018-02-08 11:48:14 +0300

    Продолжила стилизовать главное меню

--- source/js/main.js
+++ source/js/main.js
@@ -1,22 +1,24 @@
 var header = document.querySelector(".page-header");
 var toggle = document.querySelector(".main-nav__toggle");
 var menu = document.querySelector(".main-nav__list");
+var menuHeader = document.querySelector(".main-nav__header");
 
 header.classList.remove("page-header--nojs");
-header.classList.add("page-header--compact");
+header.classList.add("page-header--minimized");
 menu.classList.remove("main-nav__list--nojs");
-menu.classList.add("main-nav__list--compact");
+menu.classList.add("main-nav__list--minimized");
+menuHeader.classList.add("main-nav__header--minimized");
 
 toggle.addEventListener("click", function(event) {
   event.preventDefault();
-  if (menu.classList.contains("main-nav__list--compact")) {
-    header.classList.remove("page-header--compact");
-    header.classList.add("page-header--full");
-    menu.classList.remove("main-nav__list--compact");
+  if (menu.classList.contains("main-nav__list--minimized")) {
+    header.classList.remove("page-header--minimized");
+    menu.classList.remove("main-nav__list--minimized");
+    menuHeader.classList.remove("main-nav__header--minimized");
   }
   else {
-    header.classList.add("page-header--compact");
-    header.classList.remove("page-header--full");
-    menu.classList.add("main-nav__list--compact");
+    header.classList.add("page-header--minimized");
+    menu.classList.add("main-nav__list--minimized");
+    menuHeader.classList.add("main-nav__header--minimized");
   }
 });
